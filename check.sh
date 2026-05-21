984b0ae1558d052db8384e189e0b2d000fa6c0fb
Finlay Christ <finlaytheberry@gmail.com>
2026-05-21 02:43:18 -0700

    Fixed bug in check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 reset
-tail -n 200 log.txt | while read -r line; do
+tail -n 200 ./log.txt | while read -r line; do
   if [[ "$line" == *"ERROR - "* ]] || [[ "$line" == *"PY_EX - "* ]]; then
     echo -e "\033[31m$line\033[0m"
   elif [[ "$line" == *"Warning - "* ]]; then
@@ -11,8 +11,8 @@ tail -n 200 log.txt | while read -r line; do
   fi
 done
 echo ""
-~/PyCluster/PyCluster.py status
+./Cluster.py status
 echo ""
-LAST_BACKUP=$(ls -S ~/ONIDbot/backups/ | head -n 1 | sed 's/\.json$//')
+LAST_BACKUP=$(ls -S ./backups/ | head -n 1 | sed 's/\.json$//')
 echo "Last backup - $(date -d @$LAST_BACKUP)" - $(python -c "import time; print(format((time.time() - $LAST_BACKUP) / 86400, '.2f'))") days ago
 echo ""
