1db6adddfdd8b8e7e63d0446669cfd405cc3981e
Finlay Christ <finlaytheberry@gmail.com>
2026-05-27 23:46:55 -0700

    Now check.sh prints logs from all 3 log files.

--- check.sh
+++ check.sh
@@ -3,7 +3,27 @@
 cd "$(dirname "$0")"
 
 reset
-tail -n 200 ./log.txt | while read -r line; do
+tail -n 100 ./ONIDbot.log | while read -r line; do
+  if [[ "$line" == "ERROR - "* ]] || [[ "$line" == *"PY_EX - "* ]]; then
+    echo -e "\033[31m$line\033[0m"
+  elif [[ "$line" == "Warning - "* ]]; then
+    echo -e "\033[33m$line\033[0m"
+  else
+    echo -e "$line"
+  fi
+done
+echo ""
+tail -n 5 ./Verify.log | while read -r line; do
+  if [[ "$line" == "ERROR - "* ]] || [[ "$line" == *"PY_EX - "* ]]; then
+    echo -e "\033[31m$line\033[0m"
+  elif [[ "$line" == "Warning - "* ]]; then
+    echo -e "\033[33m$line\033[0m"
+  else
+    echo -e "$line"
+  fi
+done
+echo ""
+tail -n 5 ./Cluster.log | while read -r line; do
   if [[ "$line" == "ERROR - "* ]] || [[ "$line" == *"PY_EX - "* ]]; then
     echo -e "\033[31m$line\033[0m"
   elif [[ "$line" == "Warning - "* ]]; then
