1704111426620365bd759983b4a0ff5d631ce5cd
Finlay Christ <finlaytheberry@gmail.com>
2026-05-27 23:50:42 -0700

    Bump

--- check.sh
+++ check.sh
@@ -3,6 +3,8 @@
 cd "$(dirname "$0")"
 
 reset
+
+echo "ONIDbot.log"
 tail -n 100 ./ONIDbot.log | while read -r line; do
   if [[ "$line" == "ERROR - "* ]] || [[ "$line" == *"PY_EX - "* ]]; then
     echo -e "\033[31m$line\033[0m"
@@ -13,6 +15,8 @@ tail -n 100 ./ONIDbot.log | while read -r line; do
   fi
 done
 echo ""
+
+echo "Verify.log"
 tail -n 5 ./Verify.log | while read -r line; do
   if [[ "$line" == "ERROR - "* ]] || [[ "$line" == *"PY_EX - "* ]]; then
     echo -e "\033[31m$line\033[0m"
@@ -23,6 +27,8 @@ tail -n 5 ./Verify.log | while read -r line; do
   fi
 done
 echo ""
+
+echo "Cluster.log"
 tail -n 5 ./Cluster.log | while read -r line; do
   if [[ "$line" == "ERROR - "* ]] || [[ "$line" == *"PY_EX - "* ]]; then
     echo -e "\033[31m$line\033[0m"
@@ -33,8 +39,10 @@ tail -n 5 ./Cluster.log | while read -r line; do
   fi
 done
 echo ""
+
 ./Cluster.py status
 echo ""
+
 LAST_BACKUP=$(ls -S ./backups/ | head -n 1 | sed 's/\.json$//')
 echo "Last backup - $(date -d @$LAST_BACKUP)" - $(python -c "import time; print(format((time.time() - $LAST_BACKUP) / 86400, '.2f'))") days ago
 echo ""
--- diff_to_dev.sh
+++ diff_to_dev.sh
@@ -1,1 +1,1 @@
-diff -r ~/ONIDbot ~/ONIDbot_DEV -x 'env' -x '.git' -x 'Cluster.log' -x 'ONIDbot.log' -x 'database.json' -x 'backups' -x 'environment.json'
+diff -r ~/ONIDbot ~/ONIDbot_DEV -x 'env' -x '.git' -x 'Cluster.log' -x 'ONIDbot.log' -x 'Verify.log' -x 'database.json' -x 'backups' -x 'environment.json'
